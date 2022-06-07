155aa4b729e55af1073f2de8c15345fa69c04fa0
ewa1do <this.eduardovera@gmail.com>
2022-06-07 17:41:03 -0400

    Sesion 2: React components, classes vs functions components

--- hola-mundo/src/App.js
+++ hola-mundo/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/pure/greeting';
+import GreetingFunc from './components/pure/greetingFunc';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hola Mundo!</p>
+        {/* Componente propio Greeting.jsx */}
+        {/* <Greeting name="Eduardo"></Greeting> */}
+        <GreetingFunc name="Eduardo"></GreetingFunc>
       </header>
     </div>
   );
